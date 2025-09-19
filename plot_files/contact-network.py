79134aefd3dce1bf80ac29cda62698a6e8441562
owen-sullivan <opsullivan85@gmail.com>
2025-09-19 12:33:08 -0400

    fixed naming

--- plot_files/contact-network.py
+++ plot_files/contact-network.py
@@ -3,171 +3,118 @@ import sys
 sys.path.append("../PlotNeuralNet/")
 from pycore.tikzeng import *
 
-offset = 1
-cords = [
-    (-offset, offset),
-    (offset, offset),
-    (offset, -offset),
-    (-offset, -offset),
-]
+centerline_offset = 2.5
 
-legs = []
-for i, (x, y) in enumerate(cords):
-
-    z = 0
-    label = r"Footstep\\Preferences" if i == 0 else ""
-    legs.append(
-        to_Conv(
-            f"input_{i}",
-            5,
-            5,
-            offset=f"({z}, {x}, {y})",
-            height=5,
-            depth=5,
-            width=1,
-            caption=label,
-        )
-    )
-
-    z += 2.5
-    label = r"Position\\Filters" if i == 0 else ""
-    legs.append(
-        to_Conv(
-            f"filter_{i}",
-            5,
-            5,
-            offset=f"({z}, {x}, {y})",
-            height=5,
-            depth=5,
-            width=1,
-            caption=label,
-        )
-    )
-
-    legs.append(to_connection(f"input_{i}", f"filter_{i}"))
-
-    z += 2
-    label = r"Flatten\\Argmax" if i == 0 else ""
-    legs.append(
-        to_Conv(
-            f"argmax_{i}",
-            "",
-            "",
-            offset=f"({z}, {x}, {y})",
-            height=1,
-            depth=1,
-            width=1,
-            caption=label,
-        )
-    )
-
-    legs.append(to_connection(f"filter_{i}", f"argmax_{i}"))
-
-plot = [
+# defined your arch
+arch = [
     to_head(".."),
     to_cor(),
     to_begin(),
-    *legs,
-]
-
-z += 2
-plot.append(
     to_Conv(
-        f"footstep_options",
-        4,
-        "",
-        offset=f"({z}, 0, 0)",
-        height=1,
-        depth=4,
+        "terrain_input",
+        5,
+        5,
+        offset=f"(0,-{centerline_offset},0)",
+        height=5,
+        depth=5,
         width=1,
-        caption="Footstep\\\\Options",
-    )
-)
-
-plot.extend([to_connection(f"argmax_{i}", f"footstep_options") for i in range(4)])
-
-low_level = -offset * 3
-plot.append(
+        caption="Terrain\\\\Input",
+    ),
+    to_Conv(
+        "terrain_conv1",
+        3,
+        3,
+        offset=f"(2,-{centerline_offset},0)",
+        height=3,
+        depth=16,
+        width=3,
+        caption="Conv",
+    ),
+    to_connection("terrain_input", "terrain_conv1"),
+    to_Conv(
+        "terrain_dense2",
+        64,
+        "",
+        offset=f"(5,-{centerline_offset},0)",
+        height=1,
+        depth=64,
+        width=1,
+        caption="Dense",
+    ),
+    to_connection("terrain_conv1", "terrain_dense2"),
     to_Conv(
         "state_input",
         43,
         "",
-        offset=f"({z},{-offset*3},0)",
+        offset=f"(0,{centerline_offset},0)",
         height=1,
         depth=43,
         width=1,
         caption="State\\\\Input",
     ),
-)
-
-z += 3
-plot.append(
     to_Conv(
-        "dense1",
+        "state_dense1",
         64,
         "",
-        offset=f"({z},{low_level/2},0)",
+        offset=f"(2,{centerline_offset},0)",
         height=1,
         depth=64,
         width=1,
         caption="Dense",
     ),
-)
-
-plot.append(to_connection("state_input", "dense1"))
-plot.append(to_connection("footstep_options", "dense1"))
-
-z += 1.5
-plot.append(
+    to_connection("state_input", "state_dense1"),
     to_Conv(
-        "dense2",
+        "state_dense2",
         64,
         "",
-        offset=f"({z},{low_level/2},0)",
+        offset=f"(5,{centerline_offset},0)",
         height=1,
         depth=64,
         width=1,
         caption="Dense",
     ),
-)
-plot.append(to_connection("dense1", "dense2"))
-
-z += 1.5
-plot.append(
+    to_connection("state_dense1", "state_dense2"),
     to_Conv(
-        "dense3",
+        "conn_dense1",
         64,
         "",
-        offset=f"({z},{low_level/2},0)",
+        offset=f"(9,0,0)",
         height=1,
         depth=64,
         width=1,
         caption="Dense",
     ),
-)
-plot.append(to_connection("dense2", "dense3"))
-
-z += 1.5
-plot.append(
+    to_connection("state_dense2", "conn_dense1"),
+    to_connection("terrain_dense2", "conn_dense1"),
     to_Conv(
-        "output",
-        16,
-        "",
-        offset=f"({z},{low_level/2},0)",
-        height=1,
-        depth=16,
+        "conn_output2",
+        5,
+        5,
+        offset=f"(11, 0,0)",
+        height=5,
+        depth=5,
         width=1,
-        caption="Output",
+        caption="Preference\\\\Output",
     ),
-)
-plot.append(to_connection("dense3", "output"))
-
-plot.append(to_end())
+    to_connection("conn_dense1", "conn_output2"),
+    to_Conv(
+        "test",
+        5,
+        5,
+        offset=f"(15, 0,0)",
+        height=5,
+        depth=5,
+        width=1,
+        caption="",
+    ),
+    to_connection("conn_output2", "test"),
+    to_end(),
+]
 
 
 def main():
     namefile = str(sys.argv[0]).split(".")[0]
-    to_generate(plot, namefile + ".tex")
+    to_generate(arch, namefile + ".tex")
 
 
 if __name__ == "__main__":
--- plot_files/gait-network.py
+++ plot_files/gait-network.py
@@ -3,118 +3,171 @@ import sys
 sys.path.append("../PlotNeuralNet/")
 from pycore.tikzeng import *
 
-centerline_offset = 2.5
+offset = 1
+cords = [
+    (-offset, offset),
+    (offset, offset),
+    (offset, -offset),
+    (-offset, -offset),
+]
 
-# defined your arch
-arch = [
+legs = []
+for i, (x, y) in enumerate(cords):
+
+    z = 0
+    label = r"Footstep\\Preferences" if i == 0 else ""
+    legs.append(
+        to_Conv(
+            f"input_{i}",
+            5,
+            5,
+            offset=f"({z}, {x}, {y})",
+            height=5,
+            depth=5,
+            width=1,
+            caption=label,
+        )
+    )
+
+    z += 2.5
+    label = r"Position\\Filters" if i == 0 else ""
+    legs.append(
+        to_Conv(
+            f"filter_{i}",
+            5,
+            5,
+            offset=f"({z}, {x}, {y})",
+            height=5,
+            depth=5,
+            width=1,
+            caption=label,
+        )
+    )
+
+    legs.append(to_connection(f"input_{i}", f"filter_{i}"))
+
+    z += 2
+    label = r"Flatten\\Argmax" if i == 0 else ""
+    legs.append(
+        to_Conv(
+            f"argmax_{i}",
+            "",
+            "",
+            offset=f"({z}, {x}, {y})",
+            height=1,
+            depth=1,
+            width=1,
+            caption=label,
+        )
+    )
+
+    legs.append(to_connection(f"filter_{i}", f"argmax_{i}"))
+
+plot = [
     to_head(".."),
     to_cor(),
     to_begin(),
+    *legs,
+]
+
+z += 2
+plot.append(
     to_Conv(
-        "terrain_input",
-        5,
-        5,
-        offset=f"(0,-{centerline_offset},0)",
-        height=5,
-        depth=5,
-        width=1,
-        caption="Terrain\\\\Input",
-    ),
-    to_Conv(
-        "terrain_conv1",
-        3,
-        3,
-        offset=f"(2,-{centerline_offset},0)",
-        height=3,
-        depth=16,
-        width=3,
-        caption="Conv",
-    ),
-    to_connection("terrain_input", "terrain_conv1"),
-    to_Conv(
-        "terrain_dense2",
-        64,
+        f"footstep_options",
+        4,
         "",
-        offset=f"(5,-{centerline_offset},0)",
+        offset=f"({z}, 0, 0)",
         height=1,
-        depth=64,
+        depth=4,
         width=1,
-        caption="Dense",
-    ),
-    to_connection("terrain_conv1", "terrain_dense2"),
+        caption="Footstep\\\\Options",
+    )
+)
+
+plot.extend([to_connection(f"argmax_{i}", f"footstep_options") for i in range(4)])
+
+low_level = -offset * 3
+plot.append(
     to_Conv(
         "state_input",
         43,
         "",
-        offset=f"(0,{centerline_offset},0)",
+        offset=f"({z},{-offset*3},0)",
         height=1,
         depth=43,
         width=1,
         caption="State\\\\Input",
     ),
+)
+
+z += 3
+plot.append(
     to_Conv(
-        "state_dense1",
+        "dense1",
         64,
         "",
-        offset=f"(2,{centerline_offset},0)",
+        offset=f"({z},{low_level/2},0)",
         height=1,
         depth=64,
         width=1,
         caption="Dense",
     ),
-    to_connection("state_input", "state_dense1"),
+)
+
+plot.append(to_connection("state_input", "dense1"))
+plot.append(to_connection("footstep_options", "dense1"))
+
+z += 1.5
+plot.append(
     to_Conv(
-        "state_dense2",
+        "dense2",
         64,
         "",
-        offset=f"(5,{centerline_offset},0)",
+        offset=f"({z},{low_level/2},0)",
         height=1,
         depth=64,
         width=1,
         caption="Dense",
     ),
-    to_connection("state_dense1", "state_dense2"),
+)
+plot.append(to_connection("dense1", "dense2"))
+
+z += 1.5
+plot.append(
     to_Conv(
-        "conn_dense1",
+        "dense3",
         64,
         "",
-        offset=f"(9,0,0)",
+        offset=f"({z},{low_level/2},0)",
         height=1,
         depth=64,
         width=1,
         caption="Dense",
     ),
-    to_connection("state_dense2", "conn_dense1"),
-    to_connection("terrain_dense2", "conn_dense1"),
+)
+plot.append(to_connection("dense2", "dense3"))
+
+z += 1.5
+plot.append(
     to_Conv(
-        "conn_output2",
-        5,
-        5,
-        offset=f"(11, 0,0)",
-        height=5,
-        depth=5,
+        "output",
+        16,
+        "",
+        offset=f"({z},{low_level/2},0)",
+        height=1,
+        depth=16,
         width=1,
-        caption="Preference\\\\Output",
+        caption="Output",
     ),
-    to_connection("conn_dense1", "conn_output2"),
-    to_Conv(
-        "test",
-        5,
-        5,
-        offset=f"(15, 0,0)",
-        height=5,
-        depth=5,
-        width=1,
-        caption="",
-    ),
-    to_connection("conn_output2", "test"),
-    to_end(),
-]
+)
+plot.append(to_connection("dense3", "output"))
+
+plot.append(to_end())
 
 
 def main():
     namefile = str(sys.argv[0]).split(".")[0]
-    to_generate(arch, namefile + ".tex")
+    to_generate(plot, namefile + ".tex")
 
 
 if __name__ == "__main__":
